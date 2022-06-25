dc8251fa7ccfa595e4dc7c0bbb4a01a3ef5d4b5a
Timothy Coleman <timhc22@gmail.com>
2022-06-25 22:34:45 +0100

    Get example working with Subgraph

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import * as rainSDK from "rain-sdk"; // rain SDK imported using importmap in ind
 import { ethers } from "ethers"; // ethers library imported using importmap in index.html (or in package.json)
 import { connect } from "./connect.js"; // a very basic web3 connection implementation
 
+const SUBGRAPH_ENDPOINT = rainSDK.AddressBook.getSubgraphEndpoint(80001);
+
 /**
  * Escrow Example
  * Tutorials (see Getting Started): https://docs.rainprotocol.xyz
@@ -69,23 +71,23 @@ export async function escrowExample() {
       },
     };
 
-    console.log('### Section 1: Mint erc20 Token (Admin function)');
+    console.log('### Section 1 (Admin function): Mint erc20 Token for adding to an Escrow connected to the Sale');
     console.log("Info: Minting new ERC20 with the following state:", emissionsERC20Config);
     const emissionsErc20 = await rainSDK.EmissionsERC20.deploy(signer, emissionsERC20Config);
-
-    // todo claim function will mint another token (in addition to initial supply)
-
-    console.log('Result: emissionsErc20:', emissionsErc20);
-    const TOKEN_ADDRESS = emissionsErc20.address;
+    // todo claim function will mint another token (in addition to initial supply)??
+    const EMISSIONS_ERC20_TOKEN_ADDRESS = emissionsErc20.address;
+    console.log(`Result: emissionsErc20, with address ${EMISSIONS_ERC20_TOKEN_ADDRESS}:`, emissionsErc20); // todo check what exists in addition to what is on an erc20, are erc20s through the evm 'factory'?
 
     console.log('------------------------------'); // separator
 
-    console.log('### Section 2: Add Token to Escrow and Link to Sale (Admin function)');
-    console.log("Info: Adding token to escrow and linking to Sale (be aware that anyone can do this for your Sale):", TOKEN_ADDRESS);
-    const redeemableERC20ClaimEscrow = await rainSDK.RedeemableERC20ClaimEscrow.get(SALE_ADDRESS, TOKEN_ADDRESS, signer);
-    console.log('Info: redeemableERC20ClaimEscrow:', redeemableERC20ClaimEscrow);
+    console.log('### Section 2 (Admin function): Add Token to Escrow and Link to Sale');
+    console.log(`Info: Adding Token (${EMISSIONS_ERC20_TOKEN_ADDRESS}) to Escrow and linking to Sale (${SALE_ADDRESS}) (be aware that anyone can do this for any Sale):`);
+    const redeemableERC20ClaimEscrow = await rainSDK.RedeemableERC20ClaimEscrow.get(SALE_ADDRESS, EMISSIONS_ERC20_TOKEN_ADDRESS, signer);
+    const ESCROW_ADDRESS = redeemableERC20ClaimEscrow.address;
+    console.log(`Info: redeemableERC20ClaimEscrow, with address ${ESCROW_ADDRESS}:`, redeemableERC20ClaimEscrow);
 
-    console.log(`Info: Connecting to ERC20 token (${TOKEN_ADDRESS}) for approval of spend of ${EXAMPLE_ERC20_AMOUNT_TO_DEPOSIT}:`, );
+    // todo add 1 TOKEN_SYMBOL
+    console.log(`Info: Connecting to ${emissionsERC20Config.erc20Config.name} ERC20 token (${EMISSIONS_ERC20_TOKEN_ADDRESS}) for approval of spend of ${EXAMPLE_ERC20_AMOUNT_TO_DEPOSIT} ${emissionsERC20Config.erc20Config.symbol}:`, );
     const approveTransaction = await emissionsErc20.approve(
       redeemableERC20ClaimEscrow.address,
       ethers.utils.parseUnits(EXAMPLE_ERC20_AMOUNT_TO_DEPOSIT.toString(), EXAMPLE_ERC20_DECIMALS)
@@ -102,6 +104,13 @@ export async function escrowExample() {
 
     console.log('------------------------------'); // separator
 
+
+    console.log(`Info: Waiting 1 minute to let subgraph index data...`);
+    // wait for 1 minute so that subgraph has time to index
+    await new Promise(resolve => setTimeout(resolve, 60000));
+
+    console.log('------------------------------'); // separator
+
     // todo change raise complete parameters
     // todo change distributionEndForwardingAddress to an address so the claimers can take only 1 from escrow when making the claim //distributionEndForwardingAddress: "0x0000000000000000000000000000000000000000" // the rTKNs that are not sold get forwarded here (0x00.. will burn them)
     // todo add sdk version to videos
@@ -109,43 +118,78 @@ export async function escrowExample() {
     // the withdrawer should be the rTKN buyer (holder) of the sale 
     // (from @rouzwelt: my address was a buyer (holder) of the my sale contract so I can perform the withdraw with my wallet as signer, so for this example I think you need to link it with the Sale example,
     // so the signer is the buyer of rTKN and then can perform this example and withdraw from escrow, because if the signer is not a buyer of the sale, then he/she cannot withdraw)
-    console.log('### Section 3: Withdrawing Token (User function, i.e. holder of rTKN from Sale)');
-
-
+    console.log(`### Section 3 (User function): Withdrawing ${emissionsERC20Config.erc20Config.symbol} Token for User, i.e. holder of redeemable Tokens (rTKN) from Sale)`);
 
     // capturing the current supply of rTKN from the Sale at the time of deposit (just after depositing), to be used when calling the withdraw function
     // (by default this data needs to come from sg query but it is not in the scope of this example)
     const sale = await new rainSDK.Sale(SALE_ADDRESS, signer); // instantiating the Sale contract
     const rTKN = await sale.getRedeemable();  // instantiating the Sale's rTKN contract
-    const rTKN_CURRENT_SUPPLY_AT_TIME_OF_DEPOSIT = await rTKN.totalSupply(); // getting the current supply of rTKN // todo tutorial b. will focus on how to change this to use the subgraph
+    const TOTAL_RTKN_CURRENT_SUPPLY_AT_TIME_OF_DEPOSIT = await rTKN.totalSupply(); // getting the current supply of rTKN // todo tutorial b. will focus on how to change this to use the subgraph
 
-    // {
-    //   redeemableEscrowDeposits(where:{iSaleAddress:"0xbeBFc04050e4afddE68EC2EA5f105690765c1a1E", escrowAddress: ESCROW, depositorAddress: DEPOSITOR}) {
-    //   id
-    //   escrowAddress
-    //   redeemable {
-    //     id
-    //   }
-    //   depositor {
-    //     id
-    //   }
-    //   token {
-    //     id
-    //   }
-    //   tokenAddress
-    //   tokenAmount
-    //   redeemableSupply
-    // }
-    // }
+    // todo check if token address can be one of the query inputs
+    // todo DOES IT NEED TIME TO ADD THE TOKEN EVENT TO THE SUBGRAPH?? (NOTHING COMING UP WHEN PASSING IN EMISSIONS_ERC20_TOKEN_ADDRESS)
+    // todo--question what is the difference between tokenAmount and redeemableSupply?
 
 
-    console.log(`Info: withdrawing ${TOKEN_ADDRESS} from escrow:`);
+    // depositorAddress are the same in this example as we are using the same wallet for everything
+    let subgraphData = await fetch(SUBGRAPH_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          query {
+            redeemableEscrowDeposits(where: 
+              {iSaleAddress:"${SALE_ADDRESS}", escrowAddress: "${ESCROW_ADDRESS}", depositorAddress: "${address}", tokenAddress: "${EMISSIONS_ERC20_TOKEN_ADDRESS}"}
+            ) {
+              id
+              token {
+                id
+                decimals
+                name
+                symbol
+              }
+              tokenAmount
+              redeemableSupply
+            }
+          }
+        `
+      })
+    });
+
+    // the response will then come back as promise, the data of which will need to be accessed as such:
+    subgraphData = await subgraphData.json();
+    subgraphData = subgraphData.data.redeemableEscrowDeposits[0]; // should only be one here anyway. // todo--question is there potential for 'too quick' to cause it not to exist yet in the subgraph?
+
+
+    // todo catch for undefined
+
+    console.log(`Info: data from subgraph with endpoint ${SUBGRAPH_ENDPOINT}:`, subgraphData);
+
+    // todo add data about what the token is and how much
+    console.log(`Info: withdrawing ${emissionsERC20Config.erc20Config.symbol} from Escrow for User, (amount received will be based on User's holdings of ${subgraphData.token.symbol} at the time the ${emissionsERC20Config.erc20Config.symbol} deposit happened):`);
+
+    // without subgraph, so potentially inaccurate:
+    // const withdrawTransaction = await redeemableERC20ClaimEscrow.withdraw(
+    //   TOTAL_RTKN_CURRENT_SUPPLY_AT_TIME_OF_DEPOSIT  // each deposit captures the rTKN supply when being submitted on-chain (because the supply of rTKN can change at anytime by holders burning), so when calling withdraw, we need to pass rTKN supply at the time of that specific deposit to be able to perform the withdraw
+    // );
+    //
+
+    //todo can remove this:
+    console.log(`Info: Amount of rTKN called directly from contract (don't use this): ${TOTAL_RTKN_CURRENT_SUPPLY_AT_TIME_OF_DEPOSIT} vs subgraphData.redeemableSupply: ${subgraphData.redeemableSupply} vs subgraphData.tokenAmount: ${subgraphData.tokenAmount}`);
+
+    // with subgraph, redeemableSupply is available in the data of the deposit event:
     const withdrawTransaction = await redeemableERC20ClaimEscrow.withdraw(
-      rTKN_CURRENT_SUPPLY_AT_TIME_OF_DEPOSIT  // each deposit captures the rTKN supply when being submitted on-chain (because the supply of rTKN can change at anytime by holders burning), so when calling withdraw, we need to pass rTKN supply at the time of that specific deposit to be able to perform the withdraw
+      // todo may be able to get the sale rtkn decimals dynamically
+      subgraphData.redeemableSupply // each deposit captures the rTKN supply when being submitted on-chain (because the supply of rTKN can change at anytime by holders burning), so when calling withdraw, we need to pass rTKN supply at the time of that specific deposit to be able to perform the withdraw
+      // todo--question why don't I have to do:
+      // ethers.utils.parseUnits(subgraphData.redeemableSupply, subgraphData.token.decimals) // each deposit captures the rTKN supply when being submitted on-chain (because the supply of rTKN can change at anytime by holders burning), so when calling withdraw, we need to pass rTKN supply at the time of that specific deposit to be able to perform the withdraw
     );
+
     console.log(withdrawTransaction);
     const withdrawReceipt = await withdrawTransaction.wait();
-    console.log(`Info: receipt for withdrawal (please check your wallet to make sure you have the token, you may need to add the address for the token ${TOKEN_ADDRESS}):`, withdrawReceipt);
+    console.log(`Info: receipt for withdrawal (please check your wallet to make sure you have the token, you may need to add the address for the token ${EMISSIONS_ERC20_TOKEN_ADDRESS}):`, withdrawReceipt);
 
     console.log('------------------------------'); // separator
 
